ced24d02abcebc3aa86a44003217acba7bf3d4f1
paxluporum <paxluporum@gmail.com>
2026-04-08 23:00:53 -0400

    bounce counter added

--- js/GameLoop.js
+++ js/GameLoop.js
@@ -3,6 +3,7 @@ var context;
 var ball;
 var timer;
 var interval = 1000/60; //60 fps
+var counter = 0; 
 
 
 canvas = document.getElementById("canvas");
@@ -23,6 +24,7 @@ function animate()
     {
         ball.x = canvas.width - ball.radius;   // push ball back to the edge
         ball.vx *= -1;                         // reverse horizontal direction
+        counter++;
     }
 
     // BOUNCE OFF LEFT WALL
@@ -30,6 +32,7 @@ function animate()
     {
         ball.x = ball.radius;                  // push ball back to the edge
         ball.vx *= -1;                         // reverse horizontal direction
+        counter++;
     }
 
     //BOUNCE OFF BOTTOM WALL
@@ -37,6 +40,7 @@ function animate()
     {
         ball.y = canvas.height - ball.radius;     // push back to edge
         ball.vy *= -1;                            // reverse vertical direction
+        counter++;
     }
 
     // BOUNCE OFF TOP WALL
@@ -44,10 +48,16 @@ function animate()
     {
         ball.y = ball.radius;                     // push back to edge
         ball.vy *= -1;
+        counter++;
     }
 
 
     ball.draw(); // everything above this does not visually appear untul this function is called
-}
 
+    // Display
+    context.fillStyle = "black";                  // text color
+    context.font = "bold 28px Arial";             // text style and size
+    context.fillText("Bounces: " + counter, 20, 50);  // text + position
+}
+console.log("Current bounces:", bounceCount);
 
